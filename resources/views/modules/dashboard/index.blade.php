e17e2edafea925af4cf31da37279e1dd99aaa3d4
cornnuclear <apocalypsix@gmail.com>
2021-10-06 01:10:59 +0700

    draft dashboard

--- resources/views/modules/dashboard/index.blade.php
+++ resources/views/modules/dashboard/index.blade.php
@@ -5,24 +5,82 @@
 @endsection
 
 @section('content')
-    <!-- Main charts -->
-     test
-    <!-- /main charts -->
+    <div class="row">
+        <div class="col-sm-6 col-xl-3" style="cursor:pointer">
+            <div class="card card-body has-bg-image">
+                <div class="media">
+                    <div class="media-body">
+                        <h3>List Assignment Unit</h3><br>
+                        <h3 class="mb-0">389,438</h3>
+                    </div>
 
-@endsection
+                    <div class="ml-3 align-self-center">
+                        <i class="icon-file-text2 text-success icon-4x opacity-75"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-@section('page_resources')
-    <script src="{{ asset('assets/global/js/demo_pages/dashboard.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/streamgraph.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/sparklines.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/lines.js') }}"></script>	
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/areas.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/donuts.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/bars.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/progress.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/heatmaps.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/pies.js') }}"></script>
-    <script src="{{ asset('assets/global/js/demo_charts/pages/dashboard/light/bullets.js') }}"></script>
+        <div class="col-sm-6 col-xl-3" style="cursor:pointer">
+            <div class="card card-body bg-danger text-white has-bg-image">
+                <div class="media">
+                    <div class="media-body">
+                        <h3>All MoM</h3><br>
+                        <h3 class="mb-0">389,438</h3>
+                    </div>
+
+                    <div class="ml-3 align-self-center">
+                        <i class="icon-files-empty icon-4x opacity-75"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-xl-3" style="cursor:pointer">
+            <div class="card card-body bg-danger text-white has-bg-image">
+                <div class="media">
+                    <div class="media-body">
+                        <h3>Open MoM</h3><br>
+                        <h3 class="mb-0">389,438</h3>
+                    </div>
+
+                    <div class="ml-3 align-self-center">
+                        <i class="icon-copy4 icon-4x opacity-75"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-xl-3" style="cursor:pointer">
+            <div class="card card-body bg-danger text-white has-bg-image">
+                <div class="media">
+                    <div class="media-body">
+                        <h3>Closed MoM</h3><br>
+                        <h3 class="mb-0">389,438</h3>
+                    </div>
+
+                    <div class="ml-3 align-self-center">
+                        <i class="icon-file-check icon-4x opacity-75"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card" id="section_divider">
+                <div class="card-header header-elements-inline">
+                    <h5 class="card-title">{{ $title }}</h5>
+                </div>
+
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('page_js')
